70532dbd94f18002b524fdd1d1d9d64c4b5677fc
erwinonly1612 <75136949+erwinonly1612@users.noreply.github.com>
2022-04-03 00:11:33 +0800

    Update cases.sql

--- models/cases.sql
+++ models/cases.sql
@@ -1,1 +1,1 @@
-select * from `modern-data-stack-demo.demo_dataset.covid_data` where new_recovered > new_confirmed
+select * from `dtc-de-338802.demo_dataset.covid_data` where new_recovered > new_confirmed
